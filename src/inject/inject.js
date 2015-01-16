99c369086d97e796dd3e23bdec5df8590662b8fb
Alex Foster <alexjfno1@gmail.com>
2015-01-16 14:03:07 +0000

    Making text bigger on toggle

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,12 @@
 function toggle(toggleWide) {
+  console.log(toggleWide);
   if (toggleWide) {
-    document.body.classList.remove("trello-wide-boy");
-  } else {
     document.body.classList.add("trello-wide-boy");
+  } else {
+    document.body.classList.remove("trello-wide-boy");
   }
 }
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  this[message.method].call(message.toggle);
+  this[message.method].call(this, message.toggle);
 });
